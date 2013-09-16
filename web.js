d8efebb54048218a7bec7139266fb97fc8a62257
proll <gleb.proll@gmail.com>
2013-09-16 19:41:03 +0400

    packege.json norm

--- web.js
+++ web.js
@@ -29,6 +29,10 @@ var getContent = function(url, callback) {
 
 var respond = function (req, res) {
 	// Because we use [P] in htaccess we have access to this header
+	var host = req.headers['x-forwarded-host'];
+	if(!host) {
+		host = "favestore.com";
+	}
 	url = 'http://' + req.headers['x-forwarded-host'] + req.params[0];
 	console.log(url);
 	getContent(url, function (content) {
