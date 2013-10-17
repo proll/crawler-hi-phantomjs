d3619d21500e923240d7c94d42922e24e090c83c
proll <gleb.proll@gmail.com>
2013-10-17 11:23:04 +0400

    best practices phantomjs tunning

--- phantom-server.js
+++ phantom-server.js
@@ -14,31 +14,35 @@ var requestIds = [];
 var startTime = new Date().getTime();
 
 page.onResourceReceived = function (response) {
-    if(requestIds.indexOf(response.id) !== -1) {
-        lastReceived = new Date().getTime();
-        responseCount++;
-        requestIds[requestIds.indexOf(response.id)] = null;
-    }
+		if(requestIds.indexOf(response.id) !== -1) {
+				lastReceived = new Date().getTime();
+				responseCount++;
+				requestIds[requestIds.indexOf(response.id)] = null;
+		}
 };
 page.onResourceRequested = function (request) {
-    if(requestIds.indexOf(request.id) === -1) {
-        requestIds.push(request.id);
-        requestCount++;
-    }
+		if(requestIds.indexOf(request.id) === -1) {
+				requestIds.push(request.id);
+				requestCount++;
+		}
 };
 
 // Open the page
 page.open(system.args[1], function () {});
 
 var checkComplete = function () {
-  // We don't allow it to take longer than 5 seconds but
-  // don't return until all requests are finished
-  if((new Date().getTime() - lastReceived > 300 && requestCount === responseCount) || new Date().getTime() - startTime > 50000)  {
-    clearInterval(checkCompleteInterval);
-    console.log(page.content);
-    page.release();
-    phantom.exit();
-  }
+	// We don't allow it to take longer than 5 seconds but
+	// don't return until all requests are finished
+	if((new Date().getTime() - lastReceived > 300 && requestCount === responseCount) || new Date().getTime() - startTime > 50000)  {
+		clearInterval(checkCompleteInterval);
+		console.log(page.content.length);
+		if(!!page.release) {
+			page.release();
+		} else if(!!page.close) {
+			page.close();
+		}
+		phantom.exit();
+	}
 }
 // Let us check to see if the page is finished rendering
 var checkCompleteInterval = setInterval(checkComplete, 1);
